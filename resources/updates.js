792f378e81e7390bdd97682583f615bc352d7ee5
Anaminus <anaminus@protonmail.com>
2018-09-25 07:36:15 -0500

    Expand latest updates when no updates are targeted.

--- resources/updates.js
+++ resources/updates.js
@@ -1,4 +1,5 @@
 "use strict";
+
 function toggleList(event) {
 	let parent = event.target.closest(".update");
 	if (parent === null) {
@@ -14,6 +15,7 @@ function toggleList(event) {
 		list.classList.add("hidden");
 	};
 };
+
 function toggleAll(show, scroll) {
 	let scrollTo;
 	for (let item of document.querySelectorAll("#update-list > li .patch-list")) {
@@ -35,6 +37,22 @@ function toggleAll(show, scroll) {
 		scrollTo.scrollIntoView(true);
 	};
 };
+
+function parseDate(t) {
+	if (t === null) {
+		return null;
+	};
+	let datetime = t.attributes.datetime;
+	if (datetime === undefined) {
+		return null;
+	};
+	let p = datetime.value.match(/^(\d\d\d\d)\-(\d\d)\-(\d\d) (\d\d):(\d\d):(\d\d)/);
+	if (p === null) {
+		return null;
+	};
+	return new Date(p[1], p[2]-1, p[3], p[4], p[5], p[6]);
+};
+
 document.addEventListener("DOMContentLoaded", function(event) {
 	let controls = document.getElementById("update-controls");
 	if (controls !== null) {
@@ -65,5 +83,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	let style = document.getElementById("updates-style");
 	if (style !== null) {
 		style.sheet.insertRule(".patch-list-toggle {cursor: pointer;}");
+	};
+
+	if (!document.querySelector(".update > a.anchor:target")) {
+		// No specific update is being targeted; expand latest updates.
+		let day = 86400000; // ms
+		let latest = null;
+		for (let update of document.querySelectorAll("#update-list .update")) {
+			let list = update.querySelector(".patch-list");
+			if (list === null) {
+				continue;
+			};
+			let date = parseDate(update.querySelector("time"));
+			if (date === null) {
+				continue;
+			};
+			if (latest === null) {
+				latest = date;
+			};
+			if (latest.getTime() - date.getTime() <= day) {
+				list.classList.remove("hidden");
+			};
+		};
 	};
 });
