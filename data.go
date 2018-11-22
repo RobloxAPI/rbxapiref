c9bdc9f2c571beb764b011a82aeab645e910c163
Anaminus <anaminus@protonmail.com>
2018-11-22 12:15:07 -0600

    Move BuildInfo struct to top.

--- data.go
+++ data.go
@@ -37,6 +37,29 @@ type Build struct {
 	API    *rbxapijson.Root
 }
 
+type BuildInfo struct {
+	Hash    string
+	Date    time.Time
+	Version fetch.Version
+}
+
+func (a BuildInfo) Equal(b BuildInfo) bool {
+	if a.Hash != b.Hash {
+		return false
+	}
+	if a.Version != b.Version {
+		return false
+	}
+	if !a.Date.Equal(b.Date) {
+		return false
+	}
+	return true
+}
+
+func (m BuildInfo) String() string {
+	return fmt.Sprintf("%s; %s; %s", m.Hash, m.Date, m.Version)
+}
+
 type Patch struct {
 	Stale   bool       `json:"-"`
 	Prev    *BuildInfo `json:",omitempty"`
@@ -449,27 +472,3 @@ func (data *Data) GenerateTree() {
 		return data.TreeRoots[i].ID < data.TreeRoots[j].ID
 	})
 }
-
-type BuildInfo struct {
-	Hash    string
-	Date    time.Time
-	Version fetch.Version
-}
-
-func (a BuildInfo) Equal(b BuildInfo) bool {
-	if a.Hash != b.Hash {
-		return false
-	}
-	if a.Version != b.Version {
-		return false
-	}
-	if !a.Date.Equal(b.Date) {
-		return false
-	}
-	return true
-}
-
-func (m BuildInfo) String() string {
-	return fmt.Sprintf("%s; %s; %s", m.Hash, m.Date, m.Version)
-}
-
