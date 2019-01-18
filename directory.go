fe84d35af6a139b69bdd97c9ef619825a624daae
Anaminus <anaminus@protonmail.com>
2019-01-18 10:18:21 -0600

    Refactor entire package.
    Queries are now made generically through the Section interface.
    Sections representing directories and markdown files have been
    implemented.

---
+++ directory.go
@@ -0,0 +1,105 @@
+package rbxapidoc
+
+import (
+	"html/template"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+// FileHandler hooks into a directory query, transforming a file within the
+// directory into a Section.
+//
+// The dir argument is the directory in which the file is located. The info
+// argument is information about the current file. The query argument is the
+// current query being made.
+//
+// The handler should return nil when the query does not match, or the
+// information given about the file is inapplicable.
+type FileHandler func(dir string, info os.FileInfo, query string) Section
+
+// DirectorySection represents a directory in a file system.
+type DirectorySection struct {
+	Path     string
+	Handlers []FileHandler
+}
+
+// NewDirectorySection returns a new DirectorySection from the given path.
+// Optional handlers may be specified.
+func NewDirectorySection(path string, handlers ...FileHandler) *DirectorySection {
+	return &DirectorySection{Path: path, Handlers: handlers}
+}
+
+func (s DirectorySection) Name() string {
+	return filepath.Base(s.Path)
+}
+
+func (s DirectorySection) query(queries ...string) (section Section, next []string, isfile bool) {
+	if len(queries) == 0 {
+		return nil, nil, false
+	}
+	query, next := queries[0], queries[1:]
+	files, err := ioutil.ReadDir(s.Path)
+	if err != nil {
+		return nil, nil, false
+	}
+	for _, info := range files {
+		// Try each handler.
+		for _, handler := range s.Handlers {
+			if section = handler(s.Path, info, query); section != nil {
+				return section, next, true
+			}
+		}
+		// Try subdirectory.
+		if info.IsDir() && query == info.Name() {
+			section = &DirectorySection{
+				Path:     filepath.Join(s.Path, info.Name()),
+				Handlers: s.Handlers,
+			}
+			break
+		}
+	}
+	return section, next, false
+}
+
+// Query queries a file of the given name from the directory. For each file of
+// the directory, each FileHandler is called in order. If a handler returns a
+// non-nil Section, then that section becomes the result.
+//
+// If no handler returns a section, and the current file is a directory whose
+// name matches the query, then the result will be a DirectorySection that
+// inherits the handlers of the current section.
+//
+// Subsequent names are queried from the resulting section, if one exists.
+func (s DirectorySection) Query(name ...string) Section {
+	section, next, _ := s.query(name...)
+	if section == nil {
+		return nil
+	}
+	if len(next) > 0 {
+		return section.Query(next...)
+	}
+	return section
+}
+
+func (s DirectorySection) Subsections() []Section {
+	files, err := ioutil.ReadDir(s.Path)
+	if err != nil {
+		return nil
+	}
+	var sections []Section
+	for _, info := range files {
+		if !info.IsDir() {
+			continue
+		}
+		sections = append(sections, &DirectorySection{
+			Path:     filepath.Join(s.Path, info.Name()),
+			Handlers: s.Handlers,
+		})
+	}
+	return sections
+}
+
+func (s DirectorySection) Render() template.HTML {
+	return template.HTML(s.Path)
+}
---
+++ markdown.go
@@ -0,0 +1,184 @@
+package rbxapidoc
+
+import (
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/ast"
+	"github.com/gomarkdown/markdown/html"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+// MarkdownSection represents a portion of a markdown document. Sections are
+// delimited by headings.
+type MarkdownSection struct {
+	// Heading is the name of the outer heading enclosing the section.
+	Heading string
+	// Level is the level of the outer heading enclosing the section.
+	Level int
+	// Document is the raw content of the section.
+	Document *ast.Document
+	// Sections contains each subsection.
+	Sections []*MarkdownSection
+	// Renderer specifies a custom renderer to use when rendering the section
+	// content to HTML. If nil, the default HTML renderer is used.
+	Renderer markdown.Renderer
+}
+
+// MarkdownFileHandler is a FileHandler that parses a markdown file.
+func MarkdownFileHandler(dir string, info os.FileInfo, query string) Section {
+	if info.IsDir() {
+		return nil
+	}
+	ext := filepath.Ext(info.Name())
+	if ext != ".md" {
+		return nil
+	}
+	base := filepath.Base(info.Name())
+	if base[:len(base)-len(ext)] != query {
+		return nil
+	}
+
+	b, err := ioutil.ReadFile(filepath.Join(dir, info.Name()))
+	if err != nil {
+		return nil
+	}
+	doc, ok := markdown.Parse(b, nil).(*ast.Document)
+	if !ok {
+		return nil
+	}
+	return NewMarkdownSection(doc)
+}
+
+// getHeadingText returns the text from an ast.Heading.
+func getHeadingText(heading *ast.Heading) string {
+	if len(heading.Children) != 1 {
+		return ""
+	}
+	text, ok := heading.Children[0].(*ast.Text)
+	if !ok {
+		return ""
+	}
+	return string(text.Literal)
+}
+
+func parseMarkdownSection(section *MarkdownSection, level int) {
+	children := section.Document.Children
+
+	var i int
+	var name string
+	var hasSub bool
+	for k, child := range children {
+		heading, ok := child.(*ast.Heading)
+		if !ok || heading.Level > level {
+			if ok {
+				// Section has a subsection.
+				hasSub = true
+			}
+			continue
+		}
+		sub := MarkdownSection{
+			Heading:  name,
+			Level:    level,
+			Document: &ast.Document{},
+		}
+		if i < k {
+			sub.Document.Children = children[i:k]
+		}
+		parseMarkdownSection(&sub, level+1)
+		section.Sections = append(section.Sections, &sub)
+		hasSub = false
+		i = k + 1
+		name = getHeadingText(heading)
+	}
+	sub := MarkdownSection{
+		Heading:  name,
+		Level:    level,
+		Document: &ast.Document{},
+	}
+	// `i>=len` means that the previously added section is the last, and is
+	// empty.
+	if i < len(children) {
+		sub.Document.Children = children[i:]
+		if hasSub {
+			parseMarkdownSection(&sub, level+1)
+		}
+	}
+	// `i==0` means that the section is an orphan that covers the same
+	// children as its parent, and therefore has no siblings. Sections without
+	// siblings or children are avoided to prevent infinite recursion.
+	if i > 0 || hasSub {
+		section.Sections = append(section.Sections, &sub)
+	}
+}
+
+// NewMarkdownSection creates a new MarkdownSection from an ast.Document.
+//
+// Subsections are created by outlining the headings of the document; each
+// subheading corresponds to a subsection, which can be queried by the name of
+// the heading. Also included are "orphaned" sections, which enclose parts of
+// the document without a heading. These can be queried with an empty string.
+//
+// Only headings which are direct children of the document are outlined. Note
+// that all subsections share the same underlying document. i.e. if a node
+// within a section is modified, the parent section will be affected.
+func NewMarkdownSection(document *ast.Document) *MarkdownSection {
+	section := &MarkdownSection{Document: document}
+	parseMarkdownSection(section, 1)
+	return section
+}
+
+func (s *MarkdownSection) Name() string {
+	return s.Heading
+}
+
+func (s *MarkdownSection) Query(name ...string) Section {
+	if len(name) == 0 {
+		return nil
+	}
+	for _, sub := range s.Sections {
+		if sub.Heading != name[0] {
+			continue
+		}
+		if len(name) > 1 {
+			return sub.Query(name[1:]...)
+		}
+		return sub
+	}
+	return nil
+}
+
+func (s *MarkdownSection) Subsections() []Section {
+	subs := make([]Section, len(s.Sections))
+	for i, sub := range s.Sections {
+		subs[i] = sub
+	}
+	return subs
+}
+
+func (s *MarkdownSection) Render() template.HTML {
+	renderer := s.Renderer
+	if renderer == nil {
+		renderer = html.NewRenderer(html.RendererOptions{})
+	}
+	return template.HTML(markdown.Render(s.Document, renderer))
+}
+
+func adjustHeaderLevel(root ast.Node, delta int) {
+	if root == nil {
+		return
+	}
+	if heading, ok := root.(*ast.Heading); ok {
+		heading.Level += delta
+	}
+	for _, child := range root.GetChildren() {
+		adjustHeaderLevel(child, delta)
+	}
+}
+
+// AdjustLevel adjusts the level of each heading node in the document by
+// delta. This does not affect the Level field of the section and subsections.
+func (s *MarkdownSection) AdjustLevel(delta int) {
+	adjustHeaderLevel(s.Document, delta)
+}
