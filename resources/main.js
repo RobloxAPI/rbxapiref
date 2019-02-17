d5ae4163d58b491aa8c81aa07b2efe84f2730039
Anaminus <anaminus@protonmail.com>
2019-02-17 16:20:49 -0600

    Add toggle to hide member changes in class history.

--- resources/main.js
+++ resources/main.js
@@ -1,5 +1,6 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", function() {
+
+function initTopNav() {
 	let topnav = document.getElementById("top-nav");
 	if (topnav === null) {
 		return;
@@ -9,4 +10,40 @@ document.addEventListener("DOMContentLoaded", function() {
 	};
 	window.addEventListener("scroll", updateTopNav);
 	updateTopNav();
+};
+
+function initHistoryToggle() {
+	function toggleAll(show) {
+		for (let item of document.querySelectorAll("#history > .patch-list > li")) {
+			let diffElement = item.attributes["diff-element"];
+			if (diffElement === undefined || diffElement.value === "Class" || diffElement.value === "Enum") {
+				continue;
+			};
+			if (show) {
+				item.style.display = "none";
+			} else {
+				item.style.display = "";
+			};
+		};
+	};
+
+	let controls = document.getElementById("history-controls");
+	if (controls !== null) {
+		controls.insertAdjacentHTML("beforeend", '<label><input type="checkbox" id="hide-member-changes">Hide member changes</label>');
+	};
+
+	let hideMemberChanges = document.getElementById("hide-member-changes");
+	if (hideMemberChanges !== null) {
+		hideMemberChanges.addEventListener("click", function(event) {
+			toggleAll(event.target.checked, false);
+		});
+		toggleAll(hideMemberChanges.checked, true);
+	} else {;
+		toggleAll(false, true);
+	};
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+	initTopNav();
+	initHistoryToggle();
 });
